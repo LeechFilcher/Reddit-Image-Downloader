438510e0a014bfc5711338b7b448e340f628aca3
LeechFilcher <Lukas.Duerr@deamonic.de>
2020-06-03 17:06:55 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Reddit Image Downloader
+CHANGELOG:
+==================================================
+- Initial Commit
+
+
+BEFORE YOU START:
+==================================================
+You **SHOULD** not use this software to copy and distrubute Images from Reddit. I do not suppor this, the only reason I created this bot is to automatically download images for neural networks from Reddit to speed the process up. The EXE is self contained and does not require anything else to run. If you use this for any purposes that may be mellicous its purely your doing and I'm not responsible for what is done with this software. Also this software is free and probably updated if people seem to show interest. I thought about a UI version maybe?! 
+
+If any questions arise feel free to ask me on Twitter @LeechFilcher.
+
+About Commits:
+==================================================
+I will probably not work a ton on this anymore, at least if there are no issues arising for people. This is coded pretty robustly and as long as you have an internet connection that was not banned from reddit, all should be fine.
---
+++ RedditDownloader/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RedditSharp;
+using RedditSharp.Things;
+
+namespace RedditDownloader
+{
+    class Program
+    {
+        public static int WorkLeft;
+        public static string DownloadDir;
+        public static bool AllowNsfw;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Allow Nsfw Images? (y/N)");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                AllowNsfw = true;
+            }
+
+
+            #region getSub
+            getSub:
+            var reddit = new Reddit();
+            Console.Clear();
+            Console.WriteLine("Please enter the subs name");
+            var sub = Console.ReadLine();
+            Subreddit subreddit;
+            try
+            {
+                subreddit = reddit.GetSubreddit(sub);
+            }
+            catch
+            {
+                Console.WriteLine("Could not get that sub. Press any key to continue;");
+                Console.ReadKey();
+                goto getSub;
+            }
+            #endregion
+            
+            #region getImageCount
+            getImageCount:
+            Console.Clear();
+            Console.WriteLine("Please enter how many Images of the Sub you'd like to obtain:");
+            int.TryParse(Console.ReadLine(), out var todownloadImages);
+            if (todownloadImages == 0)
+            {
+                Console.WriteLine("You need to download at least one image\n" +
+                                  "Press any key to try again");
+                Console.ReadKey();
+                goto getImageCount;
+            }
+            #endregion
+
+            #region SelectFolder
+            selectFolder:
+            Console.Clear();
+            Console.WriteLine("Please select the folder to download the images into");
+            var folderBrowserDialog1 = new FolderBrowserDialog();
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if( result == DialogResult.OK )
+            {
+                DownloadDir = folderBrowserDialog1.SelectedPath;
+            }
+            else
+            {
+                Console.WriteLine("You need to select a folder to download stuff into\n" +
+                                  "Press any key to try again");
+                Console.ReadKey();
+                goto selectFolder;
+            }
+            #endregion
+
+            #region DownloadImages
+            WorkLeft = todownloadImages;
+            Console.WriteLine($"Starting download of {todownloadImages} Images from {subreddit.Name}");
+            var thread = new Thread(() =>
+            {
+                foreach (var post in subreddit.Posts.Take(todownloadImages))
+                {
+                    if (post.NSFW && !AllowNsfw) continue;
+                    if (post.Thumbnail == null) continue;
+                    
+                    var prm = new Program();
+                    
+                    Task t = new Task(() => { prm.DownloadImageTask(post.Url.AbsoluteUri); });
+                    t.Start();
+                }
+            });
+            thread.Start();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Downloads left: " + WorkLeft);
+                Thread.Sleep(200);
+            } while (WorkLeft > 0 && thread.IsAlive);
+            #endregion
+
+            Console.Clear();
+            Console.WriteLine("Downloads left 0");
+            Console.WriteLine("Program is finished, do you want to run again?\n (y\\N)");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                goto getSub;
+            }
+        }
+
+        #region ImageDownloadTask
+        public Task DownloadImageTask(string imageUrl)
+        {
+            
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    var imageNumber = Guid.NewGuid();
+                    webClient.DownloadFileAsync(new Uri(imageUrl), DownloadDir + "\\" + imageNumber + ".png");
+                    WorkLeft--;
+                    return Task.FromResult(0);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Task.FromResult(1);
+                }
+            }
+        }
+        #endregion
+    }
+}
