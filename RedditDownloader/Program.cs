4431087506a69d589d6f938fe8df140c67ebf119
Elias Dürr <Lukas.Duerr@deamonic.net>
2021-04-18 18:07:37 +0200

    Tons of improvement to Code Quality and Performance
    Improved Code Quality and Performance a lot by removing stuff like GoTo Statements, creating excessive amounts of tasks and other things. Cleaned up UI and created a seperate UI Thread to prevent flashing of screen when upating progress. Also Fixed bug where software would download reddit posts content or would run into exception because file already existed.

--- RedditDownloader/Program.cs
+++ RedditDownloader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,119 +14,218 @@ namespace RedditDownloader
 {
     class Program
     {
+        //Amount of work that is left
         public static int WorkLeft;
+        //The Directory to download the files into
         public static string DownloadDir;
+        //If images flagged as "NSFW" should be allowed for the download.
         public static bool AllowNsfw;
+
+        /// <summary>
+        /// Main Program Call
+        /// </summary>
+        /// <param name="args">Start arguments</param>
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Allow Nsfw Images? (y/N)");
-            if (Console.ReadKey().Key == ConsoleKey.Y)
+            #region UserInput
+
+            //Check if NSFW Images are allowed.
+            while (true)
             {
-                AllowNsfw = true;
+                Console.WriteLine("Do you want to verify images are douplicates or not or do you want to go streight up to training?");
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Y:
+                        AllowNsfw = true;
+                        break;
+                    case ConsoleKey.N:
+                        break;
+                    case ConsoleKey.Enter:
+                        break;
+                    default:
+                        Console.WriteLine("Unexpected input detected please try again.");
+                        continue;
+                }
+                break;
             }
 
-
-            #region getSub
-            getSub:
-            var reddit = new Reddit();
-            Console.Clear();
-            Console.WriteLine("Please enter the subs name");
-            var sub = Console.ReadLine();
+            //Get our Subreddit
             Subreddit subreddit;
-            try
+            while (true)
             {
-                subreddit = reddit.GetSubreddit(sub);
-            }
-            catch
-            {
-                Console.WriteLine("Could not get that sub. Press any key to continue;");
-                Console.ReadKey();
-                goto getSub;
-            }
-            #endregion
-            
-            #region getImageCount
-            getImageCount:
-            Console.Clear();
-            Console.WriteLine("Please enter how many Images of the Sub you'd like to obtain:");
-            int.TryParse(Console.ReadLine(), out var todownloadImages);
-            if (todownloadImages == 0)
-            {
-                Console.WriteLine("You need to download at least one image\n" +
-                                  "Press any key to try again");
-                Console.ReadKey();
-                goto getImageCount;
-            }
-            #endregion
+                var reddit = new Reddit();
+                Console.Clear();
+                Console.WriteLine("Please enter the subs name");
+                var sub = Console.ReadLine();
 
-            #region SelectFolder
-            selectFolder:
-            Console.Clear();
-            Console.WriteLine("Please select the folder to download the images into");
-            var folderBrowserDialog1 = new FolderBrowserDialog();
-            // Show the FolderBrowserDialog.
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if( result == DialogResult.OK )
-            {
-                DownloadDir = folderBrowserDialog1.SelectedPath;
+                try
+                {
+                    subreddit = reddit.GetSubreddit(sub);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Could not get that sub. Press any key to continue;");
+                    Console.ReadKey();
+                    continue;
+                }
             }
-            else
+
+            //Get the to download images
+            int todownloadImages;
+            while (true)
             {
+                Console.Clear();
+                Console.WriteLine("Please enter how many Images of the Sub you'd like to obtain:");
+                int.TryParse(Console.ReadLine(), out todownloadImages);
+                if (todownloadImages == 0)
+                {
+                    Console.WriteLine("You need to download at least one image\n" +
+                                      "Press any key to try again");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                break;
+            }
+
+            //Select our Folder
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select the folder to download the images into");
+                var folderBrowserDialog1 = new FolderBrowserDialog();
+                // Show the FolderBrowserDialog.
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    DownloadDir = folderBrowserDialog1.SelectedPath;
+                    break;
+                }
                 Console.WriteLine("You need to select a folder to download stuff into\n" +
                                   "Press any key to try again");
                 Console.ReadKey();
-                goto selectFolder;
             }
+
             #endregion
 
             #region DownloadImages
             WorkLeft = todownloadImages;
+
             Console.WriteLine($"Starting download of {todownloadImages} Images from {subreddit.Name}");
+            bool doingWork = true;
+            //This could be considered our "uithread" to update stuff
             var thread = new Thread(() =>
             {
-                foreach (var post in subreddit.Posts.Take(todownloadImages))
+                Console.Clear();
+                while (doingWork)
                 {
-                    if (post.NSFW && !AllowNsfw) continue;
-                    if (post.Thumbnail == null) continue;
-                    
-                    var prm = new Program();
-                    Task t = new Task(() => { 
-                        prm.DownloadImageTask(post.Url.AbsoluteUri, out var wasEmpty); 
-                        });
-                    t.Start();
+                    var donework = todownloadImages - WorkLeft;
+                    if (donework == 0) continue;
+                    var percentile = todownloadImages / 100;
+                    Console.SetCursorPosition(0, 0);
+                    Console.CursorVisible = false;
+                    var progressBar = GetProgress(((float)donework / (float)todownloadImages * 100), 100);
+                    Console.WriteLine("Overall download progress of files\n" +
+                                      $"{WorkLeft} Files left\n" +
+                                      $"{progressBar}\n");
                 }
             });
             thread.Start();
-
-            do
+            //Parallel foreach through all pots
+            Parallel.ForEach(subreddit.Posts.Take(todownloadImages), (post) =>
             {
-                Console.Clear();
-                Console.WriteLine("Downloads left: " + WorkLeft);
-                Thread.Sleep(200);
-            } while (WorkLeft > 0 && thread.IsAlive);
+                if (post.NSFW && !AllowNsfw) return;
+                if (post.Thumbnail == null) return;
+
+                DownloadImage(post.Thumbnail.ToString(), out var wasEmpty);
+
+            });
+
+            doingWork = false;
+            //Rejoin thread for UI Updates.
+            thread.Join();
             #endregion
 
+            #region AskIfUserWantsRerun
             Console.Clear();
-            Console.WriteLine("Downloads left 0");
-            Console.WriteLine("Program is finished, do you want to run again?\n (y\\N)");
-            if (Console.ReadKey().Key == ConsoleKey.Y)
+            Console.WriteLine("Program finished download.");
+            while (true)
             {
-                goto getSub;
+                Console.WriteLine("Do you want to run again? (y\\N)");
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Y:
+                        Console.WriteLine("Rerunning Program");
+                        Main(args);
+                        break;
+                    case ConsoleKey.N:
+                        break;
+                    case ConsoleKey.Enter:
+                        break;
+                    default:
+                        Console.WriteLine("Unexpected input detected please try again.");
+                        continue;
+                }
+                break;
             }
+            
+
+            #endregion
         }
 
-        #region ImageDownloadTask
-        public Task DownloadImageTask(string imageUrl, out bool wasEmpty)
+        /// <summary>
+        /// Displays Progress
+        /// </summary>
+        /// <param name="currentProgress"></param>
+        /// <returns></returns>
+        public static string GetProgress(double currentProgress, int width)
         {
-            
+            var progressBar = string.Empty;
+
+            for (int i = 0; i < width; i++)
+            {
+                if (currentProgress * 100 / width > 1)
+                {
+                    progressBar += "█";
+                    currentProgress--;
+                    continue;
+                }
+
+                progressBar += "░";
+            }
+
+            progressBar += string.Empty;
+
+            return progressBar;
+        }
+
+        /// <summary>
+        /// Downloads an image via the ImageUrl and returns if it existed at all.
+        /// </summary>
+        /// <param name="imageUrl"></param>
+        /// <param name="wasEmpty"></param>
+        /// <returns></returns>
+        public static void DownloadImage(string imageUrl, out bool wasEmpty)
+        {
             using (var webClient = new WebClient())
             {
                 try
                 {
                     wasEmpty = false;
-                    var imageNumber = Guid.NewGuid();
-                    webClient.DownloadFileAsync(new Uri(imageUrl), DownloadDir + "\\" + imageNumber + ".png");
+                    Guid imageNumber = Guid.NewGuid();
+                    //Generate a really unique GUID found an issue where we could run in here protentioally.
+                    while (true)
+                    {
+                        if (File.Exists(DownloadDir + "\\" + imageNumber + ".png"))
+                            continue;
+                        break;
+                    }
+
+                    webClient.DownloadFile(imageUrl, DownloadDir + "\\" + imageNumber + ".png");
                     FileInfo fi = new FileInfo(DownloadDir + "\\" + imageNumber + ".png");
                     var size = fi.Length;
                     if (size <= 0)
@@ -134,16 +234,13 @@ namespace RedditDownloader
                         fi.Delete();
                     }
                     WorkLeft--;
-                    return Task.FromResult(0);
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
-                    Console.WriteLine(ex.Message);
+                    //Console.WriteLine(ex.Message);
                     wasEmpty = true;
-                    return Task.FromResult(1);
                 }
             }
         }
-        #endregion
     }
 }
